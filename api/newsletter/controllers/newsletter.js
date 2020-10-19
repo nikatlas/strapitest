5fd497ffc43453df5671554f8908fc2698fa27a7
Nikatlas <nikatlas@gmail.com>
2020-10-19 18:00:27 +0300

    NewsFix

--- api/newsletter/controllers/newsletter.js
+++ api/newsletter/controllers/newsletter.js
@@ -6,8 +6,8 @@ const { sanitizeEntity } = require('strapi-utils');
  * to customize this controller
  */
 function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	return re.test(String(email).toLowerCase());
 }
 
 module.exports = {
